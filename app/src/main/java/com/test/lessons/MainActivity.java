8f32b1ca9e70fa0c70ab1b2d53977bb4a978097c
Artem Smirnov <a.smirnov@labster.pro>
2017-08-31 17:37:07 +0300

    • Button. Обработка кликов.

--- app/src/main/java/com/test/lessons/MainActivity.java
+++ app/src/main/java/com/test/lessons/MainActivity.java
@@ -1,13 +1,32 @@
 package com.test.lessons;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.hello_tv);
+        Button button = findViewById(R.id.hello_btn);
+
+        button.setOnClickListener(onClickListener);
     }
+
+
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            textView.setText("Hi!");
+        }
+    };
 }
