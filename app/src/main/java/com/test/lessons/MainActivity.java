8a2fa3062f8f1f4859cbb9159c0bf592f2727bf2
Artem Smirnov <a.smirnov@labster.pro>
2017-09-04 22:52:26 +0300

    • ImageView. Установка Drawable.

--- app/src/main/java/com/test/lessons/MainActivity.java
+++ app/src/main/java/com/test/lessons/MainActivity.java
@@ -2,6 +2,7 @@ package com.test.lessons;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -17,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.image_iv);
 
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image);
-        imageView.setImageBitmap(bitmap);
+        BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), bitmap);
+
+        imageView.setImageDrawable(bitmapDrawable);
     }
 
 }
