eae40ad5135428c1d8aa408b75428ba34013376e
Artem Smirnov <a.smirnov@labster.pro>
2017-09-04 22:08:33 +0300

    • ImageView. Начало.

--- app/src/main/java/com/test/lessons/MainActivity.java
+++ app/src/main/java/com/test/lessons/MainActivity.java
@@ -2,60 +2,14 @@ package com.test.lessons;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textView;
-    private EditText editText;
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        textView = findViewById(R.id.hello_tv);
-        button = findViewById(R.id.hello_btn);
-        editText = findViewById(R.id.name_et);
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                if (editable.toString().trim().length() > 0) {
-                    button.setEnabled(true);
-                } else {
-                    button.setEnabled(false);
-                }
-            }
-        });
-
-        button.setOnClickListener(onClickListener);
     }
-
-
-    private final View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            String name = editText.getText().toString();
-
-            textView.setText(getString(R.string.name_text_format, name));
-        }
-    };
 
 }
