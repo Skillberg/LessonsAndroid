7d669b0895c7bd4a278e991870030d17d2f74252
Artem Smirnov <a.smirnov@labster.pro>
2017-09-04 23:09:09 +0300

    • ImageView. ScaleType. Center.

--- app/src/main/java/com/test/lessons/MainActivity.java
+++ app/src/main/java/com/test/lessons/MainActivity.java
@@ -1,11 +1,7 @@
 package com.test.lessons;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        ImageView imageView = findViewById(R.id.image_iv);
-
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image);
-        BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), bitmap);
-
-        imageView.setImageDrawable(bitmapDrawable);
     }
 
 }
